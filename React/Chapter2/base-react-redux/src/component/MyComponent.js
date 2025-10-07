a4b816c113eddbf78660fd466d8b26a86d46e761
TaiNguyenTN <nguyenquoctai2022nt@gmail.com>
2025-10-07 15:31:38 +0700

    Video 18

--- React/Chapter2/base-react-redux/src/component/MyComponent.js
+++ React/Chapter2/base-react-redux/src/component/MyComponent.js
@@ -3,10 +3,19 @@
 import React from "react";
 class MyComponent extends React.Component {
   state = {
-    name: "Nguyen Quoc Tai",
+    name: "TaiNguyen",
     address: "Hoi Dan IT",
     age: 18,
   };
+
+  handleClick(event) {
+    console.log(">>> Click me my button");
+    console.log("My name is: ", this.state.name);
+  }
+
+  handleOnMoveOver(event) {
+    console.log(event.pageX);
+  }
 
   //JSX
   render() {
@@ -14,6 +23,8 @@ class MyComponent extends React.Component {
       <div>
         My name is {this.state.name} and I'm from {this.state.address} and I'm{" "}
         {this.state.age} years old.
+        <button onClick={this.handleClick}>Click me</button>
+        <button onMouseMove={this.handleOnMoveOver}>Hover me</button>
       </div>
     );
   }
