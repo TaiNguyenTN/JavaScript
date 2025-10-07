3d101c43270d42ee433cfcd59436019a2aa806fa
TaiNguyenTN <nguyenquoctai2022nt@gmail.com>
2025-10-07 15:50:04 +0700

    Video 20

--- React/Chapter2/base-react-redux/src/component/MyComponent.js
+++ React/Chapter2/base-react-redux/src/component/MyComponent.js
@@ -10,11 +10,15 @@ class MyComponent extends React.Component {
 
   handleClick(event) {
     console.log(">>> Click me my button");
-    console.log("My name is: ", this.state.name);
+
+    this.setState({
+      name: "Eric",
+      age: Math.floor(Math.random() * 100 + 1),
+    });
   }
 
   handleOnMoveOver(event) {
-    console.log(event.pageX);
+    // console.log(event.pageX);
   }
 
   //JSX
@@ -23,7 +27,7 @@ class MyComponent extends React.Component {
       <div>
         My name is {this.state.name} and I'm from {this.state.address} and I'm{" "}
         {this.state.age} years old.
-        <button onClick={this.handleClick}>Click me</button>
+        <button onClick={(event) => this.handleClick(event)}>Click me</button>
         <button onMouseMove={this.handleOnMoveOver}>Hover me</button>
       </div>
     );
